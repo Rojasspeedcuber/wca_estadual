416273170191aa2c0274ce7dafb754dab0908d01
Henrique Rojas <henriquerojinhas@gmail.com>
2026-08-03 13:36:59 -0300

    config: prepare Angular app for Easypanel deploy on rojasdev.cloud
    - Update production APP_URL to https://rojasdev.cloud/
    - Change build base-href from /wca_statistics/ to /
    - Fix OAuth redirect_uri to use environment.APP_URL consistently
    - Add Dockerfile and nginx.conf to serve the SPA
    - Ignore Angular build output docs/ directory

--- website/src/environments/environment.ts
+++ website/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    APP_URL: "https://diogojs.github.io/wca_statistics/",
+    APP_URL: "https://rojasdev.cloud/",
     BACKEND_URL: "https://wcastatisticsbackend-3ofchfdn.b4a.run"
 };
