15a7736025425275db73c9ed2d12a64956827e81
Henrique Rojas <henriquerojinhas@gmail.com>
2026-08-03 13:50:59 -0300

    config: change production URL to wcaestadual.rojasdev.cloud

--- website/src/environments/environment.ts
+++ website/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    APP_URL: "https://rojasdev.cloud/",
+    APP_URL: "https://wcaestadual.rojasdev.cloud/",
     BACKEND_URL: "https://wcastatisticsbackend-3ofchfdn.b4a.run"
 };
