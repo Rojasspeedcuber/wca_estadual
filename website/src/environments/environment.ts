ce4d2466e49abf8d9a580fbf5fe348f850007eb3
Henrique Rojas <henriquerojinhas@gmail.com>
2026-08-03 14:03:56 -0300

    config: point frontend to new backend API at api.wcaestadual.rojasdev.cloud

--- website/src/environments/environment.ts
+++ website/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     APP_URL: "https://wcaestadual.rojasdev.cloud/",
-    BACKEND_URL: "https://wcastatisticsbackend-3ofchfdn.b4a.run"
+    BACKEND_URL: "https://api.wcaestadual.rojasdev.cloud"
 };
