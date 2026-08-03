bf976b9e1898f5c492f8778d4b48e0af94d4ca22
Henrique Rojas <henriquerojinhas@gmail.com>
2026-08-03 13:42:45 -0300

    config: update WCA OAuth client_id for rojasdev.cloud

--- website/src/app/services/authentication.service.ts
+++ website/src/app/services/authentication.service.ts
@@ -40,7 +40,7 @@ export class AuthenticationService {
 
   login(): void {
     if (this.api_code == "" || this.api_code == undefined) {
-      let redirectUrl = `https://www.worldcubeassociation.org/oauth/authorize?client_id=OS6jVGAcxX_MwpLawxS1hRq8IVNEfu-FAthO72ARdyw&redirect_uri=${environment.APP_URL}&response_type=code&scope=public`;
+      let redirectUrl = `https://www.worldcubeassociation.org/oauth/authorize?client_id=EW4ff1JObVbs41jTTteB0GRnrDhYkL6B983yEyoJMmA&redirect_uri=${environment.APP_URL}&response_type=code&scope=public`;
       sessionStorage.setItem("is_getting_token", "true");
       window.location.href = redirectUrl;
     }
